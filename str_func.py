b71426a8beb65da3f57d591794335edb8f1f4290
Artem Moroz <deusestmaleficus@gmail.com>
2023-08-29 16:21:09 +0700

    str_func.py in feature/Task2 branch was created

---
+++ str_func.py
@@ -0,0 +1,7 @@
+def to_upper(letters: str) -> str:
+    """
+    Take string and return all letters in upper case
+    :param letters: string to transform
+    :return: string with letters in upper case
+    """
+    return letters.upper()
