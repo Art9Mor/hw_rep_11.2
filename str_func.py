c20bfc1d997f174124f2a2ac4414ee5a08a3ff55
Artem <99592441+Art9Mor@users.noreply.github.com>
2023-08-29 20:35:40 +0700

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def to_upper(letters: str) -> str:
     """
-    Take string and return this string with letters in upper case
+    Take string and return it with letters in upper case
     :param letters: string to transform
     :return: string with letters in upper case
     """
