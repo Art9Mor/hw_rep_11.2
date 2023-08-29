9a50204cc3c336b7052df3cd7c0cfcc02afe9706
Artem Moroz <deusestmaleficus@gmail.com>
2023-08-29 20:29:05 +0700

    add new function and docstrings in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,16 @@
 def to_upper(letters: str) -> str:
     """
-    Take string and return all letters in upper case
+    Take string and return this string with letters in upper case
     :param letters: string to transform
     :return: string with letters in upper case
     """
     return letters.upper()
+
+
+def to_title(letters: str) -> str:
+    """
+    Take string and return this string with title
+    :param letters: letters: string to transform
+    :return: string with title
+    """
+    return letters.title()
