9bb5c260036c21a5477b659b673c948a7e1c7678
Artem <99592441+Art9Mor@users.noreply.github.com>
2023-08-29 22:54:43 +0700

    Update str_func.py
    Another change tfor ex

--- str_func.py
+++ str_func.py
@@ -1,16 +1,7 @@
 def to_upper(letters: str) -> str:
     """
-    Take string and return it with letters in upper case
+    Take string and return this string with letters in upper case
     :param letters: string to transform
     :return: string with letters in upper case
     """
     return letters.upper()
-
-
-def to_title(letters: str) -> str:
-    """
-    Take string and return this string with title
-    :param letters: letters: string to transform
-    :return: string with title
-    """
-    return letters.title()
